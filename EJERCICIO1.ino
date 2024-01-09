16295bd77fd1ef03fb1e65ba6775d3d70822a350
NAYELY02 <156039793+NAYELY02@users.noreply.github.com>
2024-01-08 23:03:09 -0400

    Add files via upload

---
+++ EJERCICIO1.ino
@@ -0,0 +1,41 @@
+const int potPin = A0;  
+const int ledPin1 = 2;   
+const int ledPin2 = 3;  
+const int ledPin3 = 4;   
+
+int potValue;            
+
+void setup() {
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+}
+
+void loop() {
+  potValue = analogRead(potPin);
+
+
+  if (potValue >= 0 && potValue <= 500) {
+    digitalWrite(ledPin1, HIGH);
+    delay(1000);
+    digitalWrite(ledPin2, HIGH);
+    delay(1000);
+    digitalWrite(ledPin3, HIGH);
+    delay(1000);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+  }
+
+  else if (potValue > 500 && potValue <= 1000) {
+    digitalWrite(ledPin3, HIGH);
+    delay(1000);
+    digitalWrite(ledPin2, HIGH);
+    delay(1000);
+    digitalWrite(ledPin1, HIGH);
+    delay(1000);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin1, LOW);
+  }
+}
---
+++ EJERCICIO4.ino
@@ -0,0 +1,61 @@
+const int buttonPin = 2;      
+const int potPin = A0;        
+const int ledPins[] = {3, 5, 6, 9, 10};  
+const int numLeds = 5;
+
+int buttonState = 0;
+int lastButtonState = 0;
+int ledCount = 1; 
+int potValue = 0;
+int ledToShow = 0;
+int speed = 1000; 
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  potValue = analogRead(potPin);
+
+  
+  if (millis() % 1000 == 0) {
+    randomSeed(millis());  
+    ledToShow = random(numLeds);
+    showSingleLed(ledToShow);
+  }
+
+
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    ledCount = random(1, numLeds + 1);
+    showRandomLeds(ledCount);
+  }
+
+ 
+  speed = map(potValue, 0, 1023, 500, 2000);
+
+  lastButtonState = buttonState;
+}
+
+void showSingleLed(int index) {
+  for (int i = 0; i < numLeds; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+  digitalWrite(ledPins[index], HIGH);
+}
+
+void showRandomLeds(int count) {
+  for (int i = 0; i < count; i++) {
+    ledToShow = random(numLeds);
+    digitalWrite(ledPins[ledToShow], HIGH);
+  }
+
+  delay(speed);
+
+  for (int i = 0; i < numLeds; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+}
